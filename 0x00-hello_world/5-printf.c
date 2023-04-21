8dd007eeac716c25f89c7d9f075a9294ec481d99
faithadewuyi <iyanulek09@gmail.com>
2023-04-21 10:53:40 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,10 +3,10 @@
  * main - 5-printf.c
  * description - Write a C program that prints exactly with proper grammar,
  *
- * Return: Always 0 (Success)
+ *Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+	printf("with a proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
