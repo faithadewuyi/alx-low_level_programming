c45351ecf8a989274c3b3b7c39be1a6b152c4255
faithadewuyi <iyanulek09@gmail.com>
2023-05-10 13:30:07 -0700

    C-recursion

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * wildcmp - function that compare strings
+ * wildcmp - Compare strings
  * @s1: pointer to string params
  * @s2: pointer to string params
  * Return: 0
@@ -27,3 +27,4 @@ int wildcmp(char *s1, char *s2)
 		return (wildcmp(s1 + 1, s2 + 1));
 	}
 	return (0);
+}
