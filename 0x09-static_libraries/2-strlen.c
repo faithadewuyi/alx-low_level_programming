68dc9303c2b6ce62911e2b778eec67205010a588
faithadewuyi <iyanulek09@gmail.com>
2023-05-12 18:37:45 -0700

    task 0

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen - function returns length of a string
+ * @s: string to return
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int longi = 0;
+
+	while (*s != '\0')
+	{
+		longi++;
+		s++;
+	}
+
+	return (longi);
+}
