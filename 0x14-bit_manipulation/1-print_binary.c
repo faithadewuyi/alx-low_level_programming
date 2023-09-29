fdf67b285266e54d0c2327c23eff8dc2c4353c50
faithadewuyi <iyanuleke09@gmail.com>
2023-09-29 11:55:47 -0700

    Task 2

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_binary - function that prints the binary equivalent of a decimal number
+ * print_binary - function that prints the binary equivalent of a decimal num
  * @n: number to print in binary
  */
 void print_binary(unsigned long int n)
---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * get_bit - function that returns value of a bit at index in a decimal num
+ * @n: number to search
+ * @index: index of the bit
+ *
+ * Return: value of the bit
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int bit_val;
+
+	if (index > 63)
+		return (-1);
+
+	bit_val = (n >> index) & 1;
+
+	return (bit_val);
+}
